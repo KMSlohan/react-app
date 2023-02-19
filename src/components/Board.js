55cc2486c79b6d624bb2d5fca62a812bf2e8bf1c
lohan <alstjd7540@naver.com>
2023-02-19 17:33:41 +0900

    tic tac toe 승자 결정하기

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,9 +11,48 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [xIsNext, setXIsNext] = useState(true)
 
-  const status = `Next player: ${xIsNext ? 'X' : 'O'}`
+  // 승자 결정
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ] 
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index]
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
+    return null
+  }
+
+  // 승자 표시
+  const winner = calculateWinner(squares)
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status =  `Next player: ${xIsNext ? 'X' : 'O'}`
+  }
+
+
+  /**
+   * CELL CLICK EVENT
+   * 클릭시 X, O 교체 
+   * */
   const handleClick = (i) => {
     const newSquares = squares.slice()
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return
+    }
+
     newSquares[i] = xIsNext ? 'X' : 'O'
     setSquares(newSquares)
     // setXIsNext(!xIsNext) // 여러 개의 setXIsNext가 있어도 하나만 실행이 된다.
@@ -24,6 +63,8 @@ const Board = () => {
   const renderSquare = (i) => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />
   }
+
+
 
   /**
    * Class component에서는 render() 안에 return()
